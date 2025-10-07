7191d7b466a49369ae68588c8939b87eadc9ccb0
NoamFav <95364866+NoamFav@users.noreply.github.com>
2025-10-07 19:26:08 +0200

    chore (assignment): move global variables from .ino to header file serial_cmd.h

--- src/assignment/serial_cmd.h
+++ src/assignment/serial_cmd.h
@@ -3,9 +3,9 @@
 #include "config.h"
 #include "pid.h"
 
-extern float target_position;        // defined in .ino (single owner)
-extern PerformanceMetrics metrics;   // defined in .ino
-extern PIDController pid_controller; // defined in .ino
+extern float target_position;
+extern PerformanceMetrics metrics;
+extern PIDController pid_controller;
 extern bool telemetry_enabled;
 
 void processSerialInput();
